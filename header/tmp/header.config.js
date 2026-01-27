17e96f9e2bbcf75904fe1a41d251572faac62ecd
Mel M. Heravi <mel.heravi@gmail.com>
2026-01-27 16:35:38 -0500

    committed

--- header/tmp/header.config.js
+++ header/tmp/header.config.js
@@ -1,17 +1,20 @@
 window.app = window.app || {};
+
 window.app.header = {
     logo: "../../img/logo-nala-association.webp",
+
     mode: "dark", // light or dark
+
     fixed: true,   // fixed to top
 
     menu: [
-	{
-	    text: "Features",
-	    dropdown: ["Option 1", "Option 2"]
-	},
 	{ text: "Pricing" },
 	{ text: "About" },
-	{ text: "Contact" }
+	{ text: "Contact" },
+	{
+	    text: "XXX Features",
+	    dropdown: ["Option 1", "Option 2"]
+	}
     ],
     buttons: []
 };
