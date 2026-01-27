6f35f9312da385715bf1c1324303a38e5afc68c5
Mel M. Heravi <mel.heravi@gmail.com>
2026-01-27 16:26:16 -0500

    committed

--- header/tmp/header.config.js
+++ header/tmp/header.config.js
@@ -1,19 +1,17 @@
 window.app = window.app || {};
 window.app.header = {
-  logo: "https://via.placeholder.com/120x40?text=Logo",
-  mode: "light", // light or dark
-  fixed: true,   // fixed to top
-  menu: [
-    {
-      text: "Features",
-      dropdown: ["Option 1", "Option 2"]
-    },
-    { text: "Pricing" },
-    { text: "About" },
-    { text: "Contact" }
-  ],
-  buttons: [
-    { text: "Login", type: "secondary" },
-    { text: "Sign Up", type: "primary" }
-  ]
+    logo: "../../img/logo-nala-association.webp",
+    mode: "dark", // light or dark
+    fixed: true,   // fixed to top
+
+    menu: [
+	{
+	    text: "Features",
+	    dropdown: ["Option 1", "Option 2"]
+	},
+	{ text: "Pricing" },
+	{ text: "About" },
+	{ text: "Contact" }
+    ],
+    buttons: []
 };
