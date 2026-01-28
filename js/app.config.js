097025e3f7db40e08b0aff55a7951d6749b3e631
Mel M. Heravi <mel.heravi@gmail.com>
2026-01-28 09:49:42 -0500

    committed

--- js/app.config.js
+++ js/app.config.js
@@ -1,17 +1,4 @@
 window.app = window.app || {};
 
-window.app = {
-    user: {
-	id: "0009130",
-	fname: "Alex",
-	lname: "Metlof",
-	since: "2025.16.03",
-	subscriptions: {
-	    {id:0, part:"Part-I"},
-	    {id:3, part:"Part-III"}
-	]
-    }
-}
-
-window.app.baseUrl = "http://localhost/Melify/tools/webcomponents";
+window.app.baseUrl = "/Melify/tools/nala-website";
 
