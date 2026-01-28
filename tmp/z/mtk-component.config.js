6b6365ca63c8540cb9d63e6f299248d1152de1d8
Mel M. Heravi <mel.heravi@gmail.com>
2026-01-28 07:58:47 -0500

    committed

--- tmp/z/mtk-component.config.js
+++
@@ -1,9 +0,0 @@
-window.app = window.app || {};
-
-window.app.component = {
-  buttons: [
-    { id: "btnPrimary", label: "Primary", icon: "check_circle", type: "primary" },
-    { id: "btnWarning", label: "Warning", icon: "warning", type: "warning" },
-    { id: "btnDanger", label: "Danger", icon: "error", type: "danger" }
-  ]
-};
