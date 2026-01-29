f8ef36241478199838e129eee3216d645529eae4
Mel M. Heravi <mel.heravi@gmail.com>
2026-01-29 11:59:36 -0500

    committed

--- private/config/header.config.js
+++ private/config/header.config.js
@@ -6,7 +6,6 @@ window.app.header = {
     logo: { text: "MyApp", img: "img/logo-nala-association.webp" },
     
     menu: [
-	{ cname:"header", id: "parts", label: "Curriculum", href: "#parts", active: true },
 	{ cname:"header", id: "hierarchy", label: "Hierarchy", href: "#hierarchy"},
 	{ cname:"header", id: "settings", label: "Settings", href: "#settings"},
 	{ cname:"header", id: "logout", label: "Mel", href: "#logout"}
