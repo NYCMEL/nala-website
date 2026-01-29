62e319131b35fd29c6d31f652bb04c61ee4d7914
Mel M. Heravi <mel.heravi@gmail.com>
2026-01-29 07:52:11 -0500

    committed

--- js/_febe.js
+++ js/_febe.js
@@ -1,46 +1,60 @@
-(function () {
-    const topics = [
-	"header.menu.click",
-	"header.button.click",
-	"MTK-parts.click",
-	"mtk-hierarchy:resource:click"
-    ];
+class _febe {
+    constructor() {
+        this.topics = [
+            "header.menu.click",
+            "header.button.click",
+            "MTK-parts.click",
+            "mtk-hierarchy:resource:click"
+        ];
 
-    topics.forEach(topic => {
-	wc.log("_febe: subscribed to", topic);
-	
-	PubSub.subscribe(topic, onMessage);
-    });
-
-    // DEFINE ALL ACTIONS FOR A TOPIC
-    function onMessage(msg, data) {
-	wc.log("_febe: onMessage", msg, data);
-	
-	switch(msg) 
-	{
-	    case "mtk-hierarchy:resource:click":
-	    wc.log("_febe: onMessage", data.description, data.url);
-	    break;
-
-	    case "header.menu.click":
-	    MTKPager.show(data.id);
-	    break;
-
-	    case "header.button.click":
-	    MTKPager.show(data.id);
-	    break;
-
-	    case "MTK-parts.click":
-	    MTKPager.show("lessons");
-
-	    wc.timeout(function(){
-		lessonClicked(cIndex, cTitle);
-	    }, 500, 1);
-	    break;
-
-	    default:
-	    alert("_febe: DO NOT HAVE:" + msg);
-	    break;
-	}
+        this.subscribe();
     }
-})();
+
+    subscribe() {
+        this.topics.forEach(topic => {
+            wc.log("_febe: subscribed to", topic);
+            PubSub.subscribe(topic, this.onMessage.bind(this));
+        });
+    }
+
+    onMessage(msg, data) {
+        wc.log("_febe: onMessage", msg, data);
+
+        switch (msg) {
+        case "mtk-hierarchy:resource:click":
+            wc.log("_febe: onMessage", data.description, data.url);
+	    resource(data);
+            break;
+
+        case "header.menu.click":
+            MTKPager.resource(data.id);
+            break;
+
+        case "header.button.click":
+            MTKPager.resource(data.id);
+            break;
+
+        case "MTK-parts.click":
+            MTKPager.resource("lessons");
+
+            wc.timeout(() => {
+                lessonClicked(cIndex, cTitle);
+            }, 500, 1);
+            break;
+
+        default:
+            alert("_febe: DO NOT HAVE:" + msg);
+            break;
+        }
+    }
+
+    ///////////////////////////////////////////////////////
+    ///// PROCESS A RESOURCE
+    ///////////////////////////////////////////////////////
+    resource(data) {
+	console.log(">>>>>>>>", data.description);
+    }
+}
+
+/* auto-init */
+window._febe = new _febe();
