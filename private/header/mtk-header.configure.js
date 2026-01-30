62081a304f5ca9c400f12e1817240b8966a505e6
Mel M. Heravi <mel.heravi@gmail.com>
2026-01-30 14:57:26 -0500

    committed

--- private/header/mtk-header.configure.js
+++ private/header/mtk-header.configure.js
@@ -1,31 +1,29 @@
 window.app = window.app || {};
 
 window.app.header = {
-  theme: "dark",
-  logo: {
-    src: "https://via.placeholder.com/200x65?text=LOGO",
-    alt: "Company Logo",
-    href: "#"
-  },
-  menus: [
-    {
-      id: "account",
-      label: "Account",
-      type: "dropdown",
-      items: [
-        { id: "profile", label: "Profile" },
-        { id: "logout", label: "Logout" }
-      ]
+    theme: "dark",
+    logo: {
+	src: "img/logo-nala-association.webp",
+	alt: "Company Logo",
+	href: "#"
     },
-    {
-      id: "dashboard",
-      label: "Dashboard",
-      href: "#dashboard"
-    },
-    {
-      id: "settings",
-      label: "Settings",
-      href: "#settings"
-    }
-  ]
+    menus: [
+	{
+	    id: "dashboard",
+	    label: "Dashboard",
+	    href: "#dashboard"
+	}, {
+	    id: "settings",
+	    label: "Settings",
+	    href: "#settings"
+	}, {
+	    id: "account",
+	    label: "Account",
+	    type: "dropdown",
+	    items: [
+		{ id: "profile", label: "Profile" },
+		{ id: "logout", label: "Logout" }
+	    ]
+	}
+    ]
 };
