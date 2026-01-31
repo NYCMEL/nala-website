f4244769dc41135e5fdb2f2591f3cd0217e4189a
Mel M. Heravi <mel.heravi@gmail.com>
2026-01-31 08:38:29 -0500

    committed

--- header/header.js
+++ header/header.js
@@ -1,8 +1,9 @@
-alert("A");
-
+// PUBLISH ALL CLICKS
 $(".nav-link, .navbar-brand").on("click", function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    
     let msg = this.id; wc.log(msg);
     wc.publish(msg);
-
-    alert("A");
 });
