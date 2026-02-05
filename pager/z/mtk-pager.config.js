f74ca0c20e9d21a7e2f274dd9a3d1788d3d2992b
Mel M. Heravi <mel.heravi@gmail.com>
2026-02-05 15:40:08 -0500

    committed

--- pager/z/mtk-pager.config.js
+++ pager/z/mtk-pager.config.js
@@ -3,7 +3,7 @@ window.app = window.app || {};
 window.app.pager = {
     sections: {
 	"home": {
-	    url: "/repo_deploy/pages/home.html"
+	    url: "/repo_deploy/pager/z/tmp/home.html"
 	},
 	"about": {
 	    url: "/repo_deploy/pager/z/tmp/about.html"
