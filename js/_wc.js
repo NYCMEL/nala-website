99a39787c50b30489d33d4fbd8cbf50f5f489343
Mel M. Heravi <mel.heravi@gmail.com>
2026-02-12 12:37:04 -0500

    committed

--- js/_wc.js
+++ js/_wc.js
@@ -1281,3 +1281,73 @@ wc.getCurriculum = function () {
         throw err;
     });
 };
+
+/////////////////////////////////////////////////////////////////////////////////
+//// Make synchronous API call and return JSON response directly
+/////////////////////////////////////////////////////////////////////////////////
+// var response = wc.apiCall({
+//     method: "POST",
+//     url: wc.apiURL + "/api/login_api.php",
+//     body: {
+//         email: "mel@google.com",
+// 	password: "test"
+//     }
+// });
+// 
+// wc.user = wc.user || response.user;
+
+wc.apiCall = function(config) {
+    const {
+	method = 'GET',
+	url,
+	body = null,
+	headers = {}
+    } = config;
+
+    const xhr = new XMLHttpRequest();
+    
+    // Open connection as SYNCHRONOUS (false = sync)
+    xhr.open(method, url, false);
+    
+    // Set default headers
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    
+    // Set custom headers
+    for (let key in headers) {
+	xhr.setRequestHeader(key, headers[key]);
+    }
+    
+    try {
+	// Send request (this blocks until response comes back)
+	if (body) {
+	    xhr.send(JSON.stringify(body));
+	} else {
+	    xhr.send();
+	}
+	
+	// Check if successful
+	if (xhr.status >= 200 && xhr.status < 300) {
+	    try {
+		return JSON.parse(xhr.responseText);
+	    } catch (error) {
+		return {
+		    error: 'JSON Parse Error',
+		    message: error.message,
+		    response: xhr.responseText
+		};
+	    }
+	} else {
+	    return {
+		error: 'HTTP Error',
+		status: xhr.status,
+		statusText: xhr.statusText,
+		response: xhr.responseText
+	    };
+	}
+    } catch (error) {
+	return {
+	    error: 'Request Failed',
+	    message: error.message
+	};
+    }
+}
