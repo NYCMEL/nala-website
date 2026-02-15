d454e22117953e536e51ac5f2e2ec5e4f4d30afe
Mel M. Heravi <mel.heravi@gmail.com>
2026-02-15 09:12:00 -0500

    committed

--- tmp/app.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('#_header .nav-link, #_footer .nav-link').forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        PubSub.publish('navigate', link.dataset.section);
-    });
-});
-
-// Load initial section
-PubSub.publish('navigate', 'home');
