0c6d8179f1467dc5d3da25edd3e7d37dac288f7d
Mel M. Heravi <mel.heravi@gmail.com>
2026-02-16 08:59:45 -0500

    committed

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ wc.working       = false;
 app.baseUrl      = "/repo_deploy/";
 wc.isLocal       = true; /* = true SHOULD USE ALL LOCAL CONFIG FILES */
 wc.apiURL        = "https://nala-test.com";
-wc.productionURL = "https://nala-test.com";
+wc.productionURL = "https://nala-test.com" || "https://nalanetwork.com";
 
 // START TRACKING. COMMENT TO BYPASS
 wc.startInactivityTracking();
