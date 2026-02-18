6658e188667e981f3437b28f3a43a1aed168e77d
Mel M. Heravi <mel.heravi@gmail.com>
2026-02-18 09:04:12 -0500

    committed

--- pager/mtk-pager.config.js
+++ pager/mtk-pager.config.js
@@ -8,11 +8,12 @@ app.pager = {
     'dashboard': 'dashboard/index.inc.html',
     'course':    'hierarchy/index.inc.html',
     'settings':  'settings/index.inc.html',
+    'quiz':      'quiz/index.inc.html'
 };
 
 // OPTIONAL: DEFAULT SECTION TO LOAD ON INITIALIZATION
 app.pagerDefaults = {
-    initialSection: 'home',  // FIRST PAGE TO SHOW
+    initialSection: 'quiz',  // FIRST PAGE TO SHOW
     loadOnInit: true,        // ALWAYS LOAD THE FIRST PAGE ON INIT
     animationDuration: 10,
     debugMode: false
