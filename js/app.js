ec23c32da16112ca839131836d6584f5f479fdb2
Mel M. Heravi <mel.heravi@gmail.com>
2026-03-01 13:41:14 -0500

    committed

--- js/app.js
+++ js/app.js
@@ -8,15 +8,16 @@ app.quizSize = 20;
 
 // Message storage
 app.emsgs = [
-    { id: 1000, text: "Error(1000): Wrong 'Email' or 'Password' combination" },
-    { id: 1001, text: "Error(1001): Create user failed" },
-    { id: 1002, text: "Error(1002): No questions found for module" },
-    { id: 1003, text: "Error(1003): Registeration Failed!" },
+    { id: 1000, text: "Wrong 'Email' or 'Password' combination" },
+    { id: 1001, text: "Create user failed" },
+    { id: 1002, text: "No questions found for module" },
+    { id: 1003, text: "Registeration Failed!" },
 ];
 
 app.emsg = function (id) {
     const msg = this.emsgs.find(m => m.id === id);
-    return msg ? msg.text : `Message ID ${id} not found`;
+    if (!msg) return `Error(${id}): Message not found`;
+    return `Error(${id}): ${msg.text}`;
 };
 // let msg = app.emsg(1000);
 
