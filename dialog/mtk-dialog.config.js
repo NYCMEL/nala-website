7d83796e91357ee27233708d194c3c96bdadbb13
Mel M. Heravi <mel.heravi@gmail.com>
2026-03-02 07:33:45 -0500

    committed

--- dialog/mtk-dialog.config.js
+++ dialog/mtk-dialog.config.js
@@ -1,17 +1,20 @@
-{
-  "title": "Delete Record",
-  "message": "Are you sure you want to permanently delete this record? This action cannot be undone.",
-  "ariaLabel": "Confirmation Dialog",
-  "buttons": [
-    {
-      "label": "Cancel",
-      "class": "btn-link",
-      "event": "dialog.cancel"
-    },
-    {
-      "label": "Delete",
-      "class": "btn-danger",
-      "event": "dialog.delete"
-    }
-  ]
+// mtk-hierarchy configuration
+window.app = window.app || {};
+
+window.app.dialog = {
+    "title": "Delete Record",
+    "message": "Are you sure you want to permanently delete this record? This action cannot be undone.",
+    "ariaLabel": "Confirmation Dialog",
+    "buttons": [
+	{
+	    "label": "Cancel",
+	    "class": "btn-link",
+	    "event": "dialog.cancel"
+	},
+	{
+	    "label": "Delete",
+	    "class": "btn-danger",
+	    "event": "dialog.delete"
+	}
+    ]
 }
