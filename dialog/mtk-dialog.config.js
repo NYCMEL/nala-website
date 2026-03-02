66158aa9d63287b810f0bf787dd8344e427cc84f
Mel M. Heravi <mel.heravi@gmail.com>
2026-03-02 07:29:26 -0500

    committed

---
+++ dialog/mtk-dialog.config.js
@@ -0,0 +1,17 @@
+{
+  "title": "Delete Record",
+  "message": "Are you sure you want to permanently delete this record? This action cannot be undone.",
+  "ariaLabel": "Confirmation Dialog",
+  "buttons": [
+    {
+      "label": "Cancel",
+      "class": "btn-link",
+      "event": "dialog.cancel"
+    },
+    {
+      "label": "Delete",
+      "class": "btn-danger",
+      "event": "dialog.delete"
+    }
+  ]
+}
