98abd00a5a558a8e0e6bc7a49142177bf0d55b55
Mel M. Heravi <mel.heravi@gmail.com>
2026-03-07 07:08:17 -0500

    committed

--- js/_wc.js
+++ js/_wc.js
@@ -3,9 +3,8 @@ window.wcAPP = window.wcAPP || "NOT-SET";
 window.wcURL = window.wcURL || "";
 
 wc.working   = location.origin != 'http://localhost:3000';
-wc.isLocal   = false; /* = true SHOULD USE ALL LOCAL CONFIG FILES */
 wc.apiURL    = "https://nala-test.com" || "https://nalanetwork.com";
-wc.testing   = true;
+wc.testing   = false;  /* = true SHOULD USE ALL LOCAL CONFIG FILES and others */
 
 // Message storage
 wc.emsgs = [
