ba4604ff5901bbae55021a7046e862db1ebc691d
Mel M. Heravi <mel.heravi@gmail.com>
2026-05-06 19:49:33 -0400

    committed

--- card/mtk-card.config.js
+++ card/mtk-card.config.js
@@ -1,27 +1,27 @@
 window.mtkCardConfig = {
-  image: {
-    src: "img/b-card/card-1.png",
-    alt: "Original locksmith business card"
-  },
-  text: {
-    logoText: "Locksmith",
-    personName: "John Smith",
-    personTitle: "Manager",
-    companyName: "COMPANY NAME",
-    addressLine1: "123 Maine Street",
-    addressLine2: "City, ST 00000",
-    phonePrefix: "Tel:",
-    phone: "123-456-7890",
-    email: "Info@printit4less.com"
-  },
-  labels: {
-    logoText: "Locksmith",
-    personName: "John Smith",
-    personTitle: "Manager",
-    companyName: "COMPANY NAME",
-    addressLine1: "123 Maine Street",
-    addressLine2: "City, ST 00000",
-    phone: "Tel: 123-456-7890",
-    email: "Info@printit4less.com"
-  }
+    image: {
+	src: "img/b-card/card-1.png",
+	alt: "Original locksmith business card"
+    },
+    text: {
+	logoText: "Locksmith",
+	personName: "John Smith",
+	personTitle: "Manager",
+	companyName: "COMPANY NAME",
+	addressLine1: "123 Maine Street",
+	addressLine2: "City, ST 00000",
+	phonePrefix: "Tel:",
+	phone: "123-456-7890",
+	email: "Info@printit4less.com"
+    },
+    labels: {
+	logoText: "Locksmith",
+	personName: "John Smith",
+	personTitle: "Manager",
+	companyName: "COMPANY NAME",
+	addressLine1: "123 Maine Street",
+	addressLine2: "City, ST 00000",
+	phone: "Tel: 123-456-7890",
+	email: "Info@printit4less.com"
+    }
 };
