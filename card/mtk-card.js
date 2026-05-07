1c1e960186cc853389ac7a81415f20fbd940e838
Mel M. Heravi <mel.heravi@gmail.com>
2026-05-06 20:01:35 -0400

    committed

--- card/mtk-card.js
+++ card/mtk-card.js
@@ -1,3 +1,4 @@
+
 class MtkCard {
   constructor() {
     this.config = window.mtkCardConfig || {};
@@ -5,11 +6,11 @@ class MtkCard {
   }
 
   waitForElement() {
-    const timer = window.setInterval(() => {
+    const timer = setInterval(() => {
       this.element = document.querySelector(".mtk-card");
 
       if (this.element) {
-        window.clearInterval(timer);
+        clearInterval(timer);
         this.initialize();
       }
     }, 100);
@@ -18,8 +19,6 @@ class MtkCard {
   initialize() {
     this.bindText();
     this.bindImage();
-    this.registerEvents();
-    this.subscribe();
   }
 
   bindText() {
@@ -45,53 +44,8 @@ class MtkCard {
       this.config.image &&
       this.config.image.src
     ) {
-      image.setAttribute("src", this.config.image.src);
-
-      if (this.config.image.alt) {
-        image.setAttribute("alt", this.config.image.alt);
-      }
-    }
-  }
-
-  registerEvents() {
-    const clickable = this.element.querySelectorAll("[data-text]");
-
-    clickable.forEach((item) => {
-      item.setAttribute("tabindex", "0");
-
-      item.addEventListener("click", () => {
-        const payload = {
-          event: "mtk-card-click",
-          field: item.getAttribute("data-text")
-        };
-
-        if (window.wc && wc.log) {
-          wc.log(payload);
-        }
-
-        if (window.wc && wc.publish) {
-          wc.publish("mtk-card-click", payload);
-        }
-      });
-
-      item.addEventListener("keydown", (event) => {
-        if (event.key === "Enter" || event.key === " ") {
-          event.preventDefault();
-          item.click();
-        }
-      });
-    });
-  }
-
-  subscribe() {
-    if (window.wc && wc.subscribe) {
-      wc.subscribe("4-mtk-card", this.onMessage.bind(this));
-    }
-  }
-
-  onMessage(message) {
-    if (window.wc && wc.log) {
-      wc.log(message);
+      image.src = this.config.image.src;
+      image.alt = this.config.image.alt || "";
     }
   }
 }
