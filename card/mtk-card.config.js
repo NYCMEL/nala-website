56550d58810347133d0e79b48b3cb0baf241b3ff
Mel M. Heravi <mel.heravi@gmail.com>
2026-05-06 20:12:13 -0400

    committed

--- card/mtk-card.config.js
+++ card/mtk-card.config.js
@@ -6,22 +6,12 @@ window.mtkCardConfig = {
     text: {
 	logoText: "",
 	personName: "Mel M. Heravi",
-	personTitle: "Owner",
+	personTitle: "Owner/Operator",
 	companyName: "MEL'S LOCKSMITH",
 	addressLine1: "P.O.Box 1230",
 	addressLine2: "New York, NJ 10012",
 	phonePrefix: "Tel:",
 	phone: "(646) 303-1234",
 	email: "mel.locksmith@gmail.com"
-    },
-    labels: {
-	logoText: "Locksmith",
-	personName: "John Smith",
-	personTitle: "Manager",
-	companyName: "COMPANY NAME",
-	addressLine1: "123 Maine Street",
-	addressLine2: "City, ST 00000",
-	phone: "Tel: 123-456-7890",
-	email: "Info@printit4less.com"
     }
 };
